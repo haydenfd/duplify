f738628d8947d255df8ab972afeb637542bb369d
Hayden D'Souza <99377361+haydenfd@users.noreply.github.com>
2022-09-05 16:54:07 -0700

    Added successful auth, valid token
    Able to log user profile data to console

--- src/App.js
+++ src/App.js
@@ -3,28 +3,50 @@ import './App.css'
 import axios from "axios"
 import { Container, Button, TextField, List, ListItem, ListItemText } from '@mui/material';
 
+const CLIENT_ID = process.env.REACT_APP_API_KEY
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+const REDIRECT_URI = "http://localhost:3000"
+
+const SCOPES = ["user-read-email", "user-read-private"]
+const SCOPES_PARAM = SCOPES.join("%20");
+
+const getReturnedParamsFromSpotifyAuth = (hash) => {
+  const stringAfterHashtag = hash.substring(1);
+  const paramsInUrl = stringAfterHashtag.split("&");
+  const paramsSplitUp = paramsInUrl.reduce((accumulater, currentValue) => {
+    console.log(currentValue);
+    const [key, value] = currentValue.split("=");
+    accumulater[key] = value;
+    return accumulater;
+  }, {});
+
+  return paramsSplitUp;
+};
+
 const App = () => {
   
-  const CLIENT_ID = "#"
-  const CLIENT_SECRET = "#"
   const [accessToken, setAccessToken] = useState("")
   const [input, setInput] = useState("")
   const [playlists, setPlaylists] = useState([])
+  const [loggedIn, setLoggedIn] = useState("")
 
   useEffect(() => {
+    if (window.location.hash) {
+      const { access_token, expires_in, token_type } =
+        getReturnedParamsFromSpotifyAuth(window.location.hash);
+      setAccessToken(access_token)
 
-    var params = {
-      method: 'POST',
-      headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-      body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+      localStorage.clear();
+
+      localStorage.setItem("accessToken", access_token);
+      localStorage.setItem("tokenType", token_type);
+      localStorage.setItem("expiresIn", expires_in);
     }
+  });
 
-    fetch('https://accounts.spotify.com/api/token', params)
-    .then(result => result.json())
-    .then(data => setAccessToken(data.access_token))
-    }, [])
+  const handleLogin = () => {
+    window.location = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPES_PARAM}&response_type=token&show_dialog=true`;
+  };
 
   async function retrieveUser() { 
 
@@ -37,17 +59,33 @@ const App = () => {
       }
     }
     
-    var userObject = await fetch('https://api.spotify.com/v1/users/' + input + '/playlists', params)
-      .then(response => response.json()).then(data => setPlaylists(data.items))
+    var userObject = await fetch('https://api.spotify.com/v1/me/', params)
+      .then(response => response.json()).then(data => { return data })
+    
+    console.log(userObject)
+    setLoggedIn(userObject.display_name)
 
-
-    console.log(playlists)
   }
 
   return (
     <div className="App">
-      <Container>
-        <TextField label={"Text Value"} value={input} onChange={(e) => setInput(e.target.value)} />
+      <Container style={{ background: 'linear-gradient(120deg, #1DB954, #191414)' }}>
+        <TextField label={"Text Value"} value={loggedIn} />
+
+        <Button
+          variant="contained"
+          style={{
+            borderRadius: 15,
+            backgroundColor: "#1ed760",
+            color: "black",
+            fontFamily: "sans-serif"
+          }}
+          size="large"
+
+          onClick={() => handleLogin()}
+        >
+          Login
+        </Button>
 
         <Button
           variant="contained"
