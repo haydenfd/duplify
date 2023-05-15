77dc40a2d0370fe46b836584c47c07950de80903
Hayden D’Souza <haydenfd@Haydens-MBP.lan>
2023-05-15 09:57:46 -0700

    More config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,22 @@
 <script>
-    import { goto } from '$app/navigation';
+    import { accessToken } from '../utils/store';
+    import { redirect } from '@sveltejs/kit';
+    import { browser } from '$app/environment'
 
-    const redirect = true;
 
-    // if (redirect) {
-    //     goto('/login')
-    // }
+    if ($accessToken === null)
+    {
+        if (browser)
+        {
+            window.location.href = '/login'
+        }
+        
+    }
+    else {
+        if (browser) {
+            window.location.href = '/home'
+
+        }
+    }
+    
 </script>
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,8 +1,20 @@
 // defines stores for shared variables
-
 import { writable } from "svelte/store";
+import { browser } from "$app/environment";
 
-export const accessToken = writable(null);
+const defaultToken = null;
+const initialToken = browser? window.localStorage.getItem('accessToken') ?? defaultToken : defaultToken;
+
+export const accessToken = writable(initialToken)
+accessToken.subscribe((value) => {
+    if (browser)
+    {
+        window.localStorage.setItem('accessToken', value)
+    }
+});
+
+
+
 export let user = writable({});
 export let playlist = writable({});
 export let songs_uri_arr = writable([]);
