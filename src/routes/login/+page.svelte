7a3acc6c3a0838dfb539e9d4c4b07c0d982d0c80
Hayden D’Souza <haydenfd@Haydens-MBP.lan>
2023-05-17 05:56:18 -0700

    Login page transitions & cleanup

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,46 +1,84 @@
-<div>
-    <h1>Duplify</h1>
-    <a href="api/auth">Login</a>
+ <script>
+   import { goto } from '$app/navigation';
+   import { onMount } from 'svelte';
+   let pageTitle = "Duplify";
+   let pageSubtitle = "Clone the contents of other Spotify playlists to a personal playlist!";
+   function handleLogin() 
+   {
+     goto('api/auth')
+   }
+
+   onMount(() => {
+    startColorAnimation();
+  });
+
+  function startColorAnimation() 
+  {
+    const title = document.querySelector('.text-title');
+
+    let currentColor = 0;
+    const colors = ["#1DB954", "white", "#B037B6"];
+
+    setInterval(() =>
+   {
+      title.style.color = colors[currentColor];
+      currentColor = (currentColor + 1) % colors.length;
+    }, 1200);
+  }
+
+ </script>
+ 
+
+ <div>
+   <h1 class="text-title">{pageTitle}</h1>
+   <h2 class="text-subtitle">{pageSubtitle}</h2>
+   <button on:click={handleLogin} class="button-login">Login</button>
  </div>
  
+
  <style>
  
- div {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    position: absolute;
-     top: 50%;
-     left: 50%;
-     margin-top: -50px;
-     margin-left: -50px;
-     width: 100px;
-     height: 100px;
- }
+   div {
+     display: flex;
+     flex-direction: column;
+     align-items: center;
+     justify-content: center;
+     height: 80vh;
+   }
  
-  a {
-     text-decoration: none;
-     display: block;
-     width: 150px;
+   h1 {
+     font-size: 70px;
+     font-weight: 700;
+     margin-bottom: 20px;
+     text-transform: uppercase;
+     transition: color 0.28s ease-in-out;
+     color: #1DB954;
+   }
+ 
+   h2 {
+     font-size: 34px;
+     margin-bottom: 80px;
+     font-weight: 500;
+   }
+ 
+   button {
+     width: 180px;
      height: 80px;
-     background: #1DB954;
-     padding: 35px;
-     text-align: center;
-     border-radius: 5px;
-     color: white;
+     background: #1DB954;     
+     color: #fff;
+     border: none;
+     cursor: pointer;
      font-weight: bold;
-     line-height: 25px;
      font-size: 24px;
      border: 2px solid transparent;
+     border-radius: 5px;
+     font-family: 'Montserrat';
+     transition: all 0.28s ease-in-out;
+   }
+
+   button:hover {
+     background-color: #B037B6;
   }
- 
-  a:hover {
-     background-color: rgb(176, 55, 182);
-  }
- 
-  h1 {
-    font-size: 136px;
-  }
+
  </style>
+ 
