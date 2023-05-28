abe3d097e32966f56be8298f300da03d43669846
Hayden D’Souza <haydenfd@Haydens-MacBook-Pro.local>
2023-05-28 16:19:13 -0700

    Pushed some changes to fix config errors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script>
     import Navbar from "../Components/navbar.svelte";
     import { browser } from "$app/environment";
+    import { onMount } from 'svelte';
 
-    if (browser)
+    onMount(() => {
+        if (browser)
     {
+        console.log('Hello')
         if (window.sessionStorage.getItem("accessToken") === null)
         {
             window.location.href = '/login'
@@ -13,6 +16,8 @@
             window.location.href = '/home'
         }
     }
+    })
+
 
     const checkForToken = () => {
         if (browser)
