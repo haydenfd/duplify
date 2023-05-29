5d78418ac915bdf79c27975eb5e06dce0d35a300
Hayden D’Souza <haydenfd@Haydens-MacBook-Pro.local>
2023-05-29 02:36:01 -0700

    Configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,8 @@
 import adapter from '@sveltejs/adapter-vercel';
- 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  kit: {
-    adapter: adapter({
-      runtime: 'nodejs18.x',
-    }),
-  },
-};
- 
-export default config;
+
+export default {
+    kit: {
+        adapter: adapter({
+        })
+    }
+  }
