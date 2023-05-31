cecff9ce3fcf84b079e22b1bd7f124b5c5cf027b
Hayden D’Souza <haydenfd@Haydens-MacBook-Pro.local>
2023-05-30 17:02:59 -0700

    Changes to footer

--- src/Components/footer.svelte
+++ src/Components/footer.svelte
@@ -4,27 +4,55 @@
 
 
 <footer>
-    <h1 class="duplify-footer-text">
-        A Hayden D'Souza production | GitHub goes here | Email link
-    </h1>
+    <div class="flex-child">
+        <h1 class="footer-text">Footer</h1>
+    </div>
+    <div class="flex-child two-way-border">
+        <h1 class="footer-text">Footer</h1>
+    </div>
+    <div class="flex-child">
+        <h1 class="footer-text">Footer</h1>
+    </div>
+    <!-- <h1 class="duplify-footer-text"> -->
+        <!-- | A Hayden D'Souza production | <a href="#" class="a-tag">GitHub</a> |  <a href="#" class="a-tag">Email</a> | -->
+    <!-- </h1> -->
 </footer>
 
 <style>
 
     footer {
-        padding: 25px;
+        padding: 15px;
         text-align: center; 
-        width: 40vw;
+        width: 50vw;
         position: absolute;
         bottom: 0;
+        display: inline-flex;
+        flex-direction: row;
+        justify-content: space-around;
+        margin-top: 20px;
+        flex-wrap: nowrap;
+    }
+    
+    .flex-child {
+        flex: 1;
+        transform: scale(0.87);
+        height: 3rem;
     }
 
-    .duplify-footer-text {
+    .two-way-border {
+        border-right: 1px solid white;
+        border-left: 1px solid white;
+    }
+
+    .footer-text {
         color: #aaa;
-        font-size: 0.85rem;
-        display: inline-block;
-        padding: 20px;
-        border-top: 1px solid #ddd;
+        font-size: 1rem;
+    }
+
+    .a-tag {
+        font-weight: 500;
+        font-size: 1rem;
+        text-decoration: none;
     }
 
 </style>
